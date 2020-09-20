9dd2ada7da14d1281a9325cc61ed0597dd3d26ce
HenriBenz <71237474+HenriBenz@users.noreply.github.com>
2020-09-21 00:47:41 +0200

    Create Singapore.py

---
+++ Henri_Benz/Singapore.py
@@ -0,0 +1,22 @@
+
+##################################################
+# Author: Henri Benz
+# Copyright: Copyright 2020, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.0.0
+# Maintainer: Henri Benz
+# Email: hendrik.benz@students.iaac.net
+# Status: development
+##################################################
+
+# End of header section
+
+
+city_name = 'Singapore'
+city_area = '721500'
+city_population = '5639000'
+city_density = city_population/city_area
+
+print('Population Density: ' + str(city_density) + ' inhabitatants per square kilometers')
+
