f177a7f2a10b3d0536f726a5d0734e23b515e325
Arpan <67973156+zostazebistor@users.noreply.github.com>
2020-09-20 18:11:27 +0200

    Update My_City.py

--- Arpan/My_City.py
+++ Arpan/My_City.py
@@ -19,19 +19,29 @@
 
 # End of header section
 
-My_city_name = "debrecen"
+My_city_name = "Debrecen"
 My_city_population = 250000
 My_city_area = 460
 My_city_young_population = 125000
 My_city_elder_population = 75000
 My_city_older_population = 50000
 
-Population_density = My_city_population/My_city_area
-Population_density_young = My_city_young_population/My_city_area
-Population_density_elder = My_city_elder_population/My_city_area
-Population_density_older = My_city_older_population/My_city_area
+Population_density = My_city_population / My_city_area
+Population_density_young = My_city_young_population / My_city_area
+Population_density_elder = My_city_elder_population / My_city_area
+Population_density_older = My_city_older_population / My_city_area
 
+print("My City Name is =", My_city_name)
 print("My City Population density =", int(Population_density))
 print("Population density of young =", int(Population_density_young))
 print("Population density of elder =", int(Population_density_elder))
 print("Population density of older =", int(Population_density_older))
+
+age = int(input("enter you age"))
+
+if age <= 18:
+    print("You are young and counted under young population")
+elif age > 18 > 60:
+    print("You are elder and counted under elder population")
+else:
+    print("You are old and counted under old population")
