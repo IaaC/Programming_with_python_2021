d8cbb3c29ef9cb52e2da86bd00c5c31d87ea4cb5
HenriBenz <71237474+HenriBenz@users.noreply.github.com>
2020-09-21 17:13:45 +0200

    conditionals push

---
+++ Henri_Benz/conditionals.py
@@ -0,0 +1,13 @@
+
+my_city_name = 'Singapore'
+my_city_population = 5600000
+
+if my_city_population > 10000000:
+    print('My City is a Megacity')
+elif my_city_population > 1500000:
+    print('My City is a large Metropolitan Area')
+else:
+    print('My City is neither a Megacity nor a large Metropolitan Area')
+
+print('End of Script')
+
