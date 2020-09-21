fa9d3e17d6e6c48cecafc22d83c07616a5050f8d
HenriBenz <71237474+HenriBenz@users.noreply.github.com>
2020-09-21 17:26:15 +0200

    More Area Types

--- Henri_Benz/conditionals.py
+++ Henri_Benz/conditionals.py
@@ -6,6 +6,13 @@ if my_city_population > 10000000:
     print('My City is a Megacity')
 elif my_city_population > 1500000:
     print('My City is a large Metropolitan Area')
+elif my_city_population > 500000:
+    print('My City is a samll Metropolitan Area')
+elif my_city_population > 200000:
+    print('My City is a Metropolitan Area')
+elif my_city_population > 50000:
+    print('My City is a small Urban Area')
+
 else:
     print('My City is neither a Megacity nor a large Metropolitan Area')
 
