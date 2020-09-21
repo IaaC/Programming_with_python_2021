167cc687ece681901e413a4410f3d1ed5e0fee0c
Leylasaadi90 <Leylasaadi90@users.noreply.github.com>
2020-09-21 06:04:19 -0700

    test 2

---
+++ Leyla Saadi/Conditions.py
@@ -0,0 +1,32 @@
+my_city_name = 'Moscow'
+my_city_population = 11920000
+
+if my_city_population > 10000000:
+    print('My City is a Megacity\n')
+
+if my_city_population > 20000000:
+    print('My City is a really really big\n')
+elif my_city_population > 11000000:
+    print('My city is kinda big\n')
+else:
+    print('My city is NOT that big anyway\n')
+if my_city_population > 500000:
+    print('My city is a metro area\n')
+if my_city_population > 200000:
+    print('My city is still pretty big\n')
+
+
+print('End of story')
+
+
+
+
+
+
+
+#Megacity > 10000000
+#Large metrapolitan area > 1500000
+#Metrapolitan area > 500000
+#Medium size urban area > 200000
+#Small urban area > 50000
+
