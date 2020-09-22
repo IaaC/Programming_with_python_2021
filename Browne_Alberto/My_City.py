1b7395ecdc657c19fb90334118f84f77fc4e9e3f
Alberto Browne <aebrowne@gmail.com>
2020-09-22 17:12:44 +0200

    Browne_Alberto_My City

---
+++ Browne_Alberto/My_City.py
@@ -0,0 +1,38 @@
+##################################################
+# Author: Alberto Browne
+# Copyright: Copyright 2020, IAAC
+# Credits: [Institute for Advanced Architecture of Catalonia - IAAC, Advanced Architecture group]
+# License:  Apache License Version 2.0
+# Version: 1.0.0
+# Maintainer: Alberto Browne
+# Email: alberto.eugenio.browne@students.iaac.net
+# Status: development
+##################################################
+
+City_area = 121.9
+City_population = 7273
+
+# Let's assign the result of an operation
+City_density = City_population / City_area
+Factor_density = City_density / City_area
+
+# Let's print the information
+print('1. Country:')
+print('Chile')
+print('2. City:')
+print('Panquehue')
+print('Latitude: -32.7711, Longitude: -70.8367 \n\n')
+print('3. City Area:')
+print('121.9 km2')
+print('4. Population:')
+print('7.273 inhabitants \n\n')
+
+# Let's print out the result
+print('5. City Density:')
+print(City_density)
+
+# Let's print out an other result
+print('6. Factor Density:')
+print(Factor_density)
+print('........ \n\n')
+print('Thanks')
