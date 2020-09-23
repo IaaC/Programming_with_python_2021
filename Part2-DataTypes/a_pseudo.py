2ff21e6f549bcc0723e39a8f23ec41d169f03f70
Diego Pajarito <diegopajarito@gmail.com>
2020-09-23 06:14:25 -0500

    Added resources for data types

---
+++ Part2-DataTypes/a_pseudo.py
@@ -0,0 +1,24 @@
+# declaring an integer value for variable "my_data"
+# changing the value of my_data
+# checking the type of the my_data
+
+# changing the data type of my_data from integer to float
+# checking the type of the my_data
+# converting my_data to integer
+# checking the type of the my_data
+
+# using arithmetic opertations with my_data
+
+# creating different variables and using arithmetic data
+
+# reassign my_data with reference to the itself
+
+# assigning a String value for my_data
+# checking the type of the my_data
+
+# String concatenation
+# String multiplication
+
+# arithmetic operation with numbers and text
+
+#
