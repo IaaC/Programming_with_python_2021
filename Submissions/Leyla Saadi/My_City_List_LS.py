52524ac6f9a53ee5c7f79887ece7ecb76c15a887
Leyla <leyla.saadi@students.iaac.net>
2020-09-24 06:06:25 -0700

    WIP city list

---
+++ Submissions/Leyla Saadi/My_City_List_LS.py
@@ -0,0 +1,27 @@
+
+
+
+names = ['Dubai', 'Riga', 'London', 'Bucharest', 'Budapest', 'Milan', 'Lisbon', 'Porto', 'Marrakesh', 'Ubud', 'Kuala Lumpur', 'chefchauon']
+
+population = [3000000, 14000, 400000, 5603030, 324243, 33434, 43424, 700000, 300000, 1200000, 424343000, 333300]
+
+cities = [names, population]
+
+number_of_cities = len(names)
+
+print(cities[0][0], 'has a population of', cities[1][0])
+
+for i in range(number_of_cities):
+    print(cities[0][i], 'has a population of', [1][i])
+
+
+
+
+
+#if names >= 10:
+#    print(cities)
+#else:
+#    print('you have less than 10 cities')
+
+#for i in range(number_of_cities):
+#  print(cities[i])
